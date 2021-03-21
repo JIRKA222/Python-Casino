be782b3b60888b8f7e29758a547c133f3a42c556
JIRKA222 <48966182+JIRKA222@users.noreply.github.com>
2021-03-21 13:21:40 -0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 # latin-1a
+from colorama import Fore, Back, Style
 import os
 import sys
 
@@ -13,7 +14,6 @@ import russian_roulette
 
 from colorama import init
 init()
-from colorama import Fore, Back, Style
 
 if not 'pip' in sys.modules.keys():
     print("You need to install pip (https://pypi.org/project/pip/)")
@@ -28,11 +28,7 @@ if not 'colorama' in sys.modules.keys():
 
 player_name = "Player"
 player_money = 500
-casino_ascii_art = "   __            _          \n  / / _   _  ___| | ___   _ \n / / | | | |/ __| |/ / | | |\n/ /__| |_| | (__|   <| |_| |\n\____/\__,_|\___|_|\_\\__, |\n                      |___/ \n __________                 \n|___ /___  |                \n  |_ \  / /                 \n ___) |/ /                  \n|____//_/"
-russian_names = ["Sofia", "Anastasia", "Victoria", "Ksenia", "Arina", "Elizaveta", "Adelina", "Irina", "Yelena", "Polina", "Daria", "Natalia", "Svetlana", "Vera", "Nadezhda", "Galina", "Lyubov", "Aleksandra", "Maria", "Anna", "Angelina", "Marina", "Yekaterina", "Ludmila",
-                 "Tatiana", "Artyom", "Aleksandr", "Roman", "Yevgeny", "Ivan", "Maksim", "Denis", "Alexey", "Dmitry", "Danyl", "Sergey", "Nikolai", "Konstantin", "Nikita", "Mikhail", "Boris", "Victor", "Gennady", "Vyacheslav", "Vladimir", "Andrey", "Anatoly", "Ilya", "Kirill", "Oleg"]
 entered_name = False
-#debug_mode = False
 
 
 # __________Main Game Parts Things__________
@@ -53,11 +49,13 @@ def main():
 def intro():
     # console_format.library_info()
     print(Fore.YELLOW, end="")
-    print(casino_ascii_art)
+    print(console_format.casino_ascii_art)
     print(Fore.WHITE)
 
 
 def pick_game():
+    global player_money
+
     print("Select a game:")
     print("\t[A]: Russian roulette")
     print("\t[B]: Na to neklikej")
@@ -66,13 +64,9 @@ def pick_game():
 
     if game_choice_input == "A" or game_choice_input == "a":
         console_format.clear()
-        rr_main()
+        player_money += russian_roulette.rr_main(player_name, player_money)
     else:
         quit()
-
-
-def return_menu():
-    input("Press enter to return to the main menu...")
 
 
 def debug_mode_menu():
@@ -93,85 +87,6 @@ def name_menu():
     entered_name = True
     console_format.clear()
 
-
-#__________Russian Roulette__________
-
-def rr_main():
-    russian_roulette.print_intro()
-    if console_format.is_proceed_menu() == False:
-        return
-
-    global player_name
-    global player_money
-    
-    current_competetors = []
-    current_status_of_competetors = []
-    current_num_competetors = 0
-    you_won = False
-
-    current_num_competetors = console_format.input_number(
-        "How many people do you wish to compete with? (5 is reccomended): ", 1)
-    current_max_num_competetors = current_num_competetors
-
-    prize_pool = 0
-
-    for a in range(current_num_competetors):
-        current_competetors.append(rnd.choice(russian_names))
-        current_status_of_competetors.append(True)
-        prize_pool += rnd.randint(100, 1000)
-    current_competetors.append(player_name)
-    current_status_of_competetors.append(True)
-    print(f"You can win {prize_pool} monis.")
-    print(
-        f"The odds of you surviving could be {round((1 / len(current_competetors) * 100), 2)}%.")
-    console_format.format_reset()
-    input()
-
-    while True:
-        console_format.clear()
-        i = 0
-        magazine = 0
-        magazine = russian_roulette.create_magazine(
-            current_competetors, current_status_of_competetors)
-        while i <= current_max_num_competetors:
-            if current_status_of_competetors[i] == True:
-                russian_roulette.print_competetor_status(
-                    current_competetors, current_status_of_competetors)
-                print()
-                print(f"{current_competetors[i]}'s turn.", end="")
-                input()
-                console_format.print_loading_bar(10, 0.1)
-                if i == magazine:
-                    current_status_of_competetors[i] = False
-                    current_num_competetors -= 1
-
-                    console_format.clear()
-                    russian_roulette.print_competetor_status(
-                        current_competetors, current_status_of_competetors)
-                    print()
-                    print(Fore.LIGHTRED_EX + f"Bang!!!")
-                    print(Fore.WHITE, end="")
-                    break
-                else:
-                    print(f"The gun didn't fire.")
-                input()
-            console_format.clear()
-            i += 1
-        input()
-        
-        if current_num_competetors == 0 and current_status_of_competetors[len(current_competetors) - 1] == True:
-            you_won = True
-            break
-        elif current_status_of_competetors[len(current_competetors) - 1] == False:
-            you_won = False
-            break
-    if you_won == True:
-        player_money += prize_pool
-        russian_roulette.win_screen(current_competetors, current_status_of_competetors, player_money)
-        return_menu()
-    else:
-        player_money = 0
-        russian_roulette.lose_screen(current_competetors, current_status_of_competetors, player_money)
 
 while True:
     main()
