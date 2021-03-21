6af5effbadc93963a167837ecd30781b491e8bc6
JIRKA222 <48966182+JIRKA222@users.noreply.github.com>
2021-03-21 13:21:53 -0100

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -1,23 +1,23 @@
+from colorama import Fore, Back, Style
 import console_format
 import random as rnd
 
 from colorama import init
 init()
-from colorama import Fore, Back, Style
-
 
 # __________Russian Roulette__________
 
-def win_screen(win_list, win_status, player_money):
+
+def win_screen(win_list, win_status, money_amount):
     console_format.clear()
     print(Fore.GREEN +
           f"As you stand next to a pile of {len(win_list) - 1} bodies, you begin wonder: What am I gonna do with this money?")
     console_format.format_reset()
-    console_format.print_money(player_money)
+    print(f"You earned {money_amount} monis.")
     print_competetor_status(win_list, win_status)
 
 
-def lose_screen(lost_list, lost_status, player_money):
+def lose_screen(lost_list, lost_status, money_amount):
     console_format.clear()
     print(Fore.RED + "In an unfortunate turn of events, you seem to have created a new opening in your skull.")
     console_format.format_reset()
@@ -70,3 +70,82 @@ def print_intro():
     print("Are you certain it is in your best interest to proceed?")
     print("\t[A]:   Yes")
     print("\t[ANY]: No")
+
+
+def rr_main(player_name, player_money):
+    print_intro()
+    if console_format.is_proceed_menu() == False:
+        return player_money
+
+    current_competetors = []
+    current_status_of_competetors = []
+    current_num_competetors = 0
+    you_won = False
+
+    current_num_competetors = console_format.input_number(
+        "How many people do you wish to compete with? (5 is reccomended): ", 1)
+    current_max_num_competetors = current_num_competetors
+
+    prize_pool = 0
+
+    for a in range(current_num_competetors):
+        current_competetors.append(rnd.choice(console_format.russian_names))
+        current_status_of_competetors.append(True)
+        prize_pool += rnd.randint(100, 1000)
+    current_competetors.append(player_name)
+    current_status_of_competetors.append(True)
+    print(f"You can win {prize_pool} monis.")
+    print(
+        f"The odds of you surviving could be {round((1 / len(current_competetors) * 100), 2)}%.")
+    console_format.format_reset()
+    input()
+
+    while True:
+        console_format.clear()
+        i = 0
+        magazine = 0
+        magazine = create_magazine(
+            current_competetors, current_status_of_competetors)
+        while i <= current_max_num_competetors:
+            if current_status_of_competetors[i] == True:
+                print_competetor_status(
+                    current_competetors, current_status_of_competetors)
+                print()
+                print(f"{current_competetors[i]}'s turn.", end="")
+                input()
+                console_format.print_loading_bar(10, 0.1)
+                if i == magazine:
+                    current_status_of_competetors[i] = False
+                    current_num_competetors -= 1
+
+                    console_format.clear()
+                    print_competetor_status(
+                        current_competetors, current_status_of_competetors)
+                    print()
+                    print(Fore.LIGHTRED_EX + f"Bang!!!")
+                    print(Fore.WHITE, end="")
+                    break
+                else:
+                    print(f"The gun didn't fire.")
+                input()
+            console_format.clear()
+            i += 1
+        input()
+
+        if current_num_competetors == 0 and current_status_of_competetors[len(current_competetors) - 1] == True:
+            you_won = True
+            break
+        elif current_status_of_competetors[len(current_competetors) - 1] == False:
+            you_won = False
+            break
+    if you_won == True:
+        win_screen(
+            current_competetors, current_status_of_competetors, prize_pool)
+
+    else:
+        lose_screen(
+            current_competetors, current_status_of_competetors, prize_pool)
+        prize_pool = 0
+
+    input("Press enter to return to the main menu...")
+    return prize_pool
