69895228c007af094ebe2a9ac9d4b3ce1740df7e
JIRKA222 <48966182+JIRKA222@users.noreply.github.com>
2021-04-15 16:32:49 +0200

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -47,7 +47,6 @@ def print_competetor_status(competetor_list, competetor_status):
 
 
 def create_magazine(num_competetors_load, status_of_competetors_load):
-    magazine = []
     ammo_pos = 0
     while True:
         ammo_pos = rnd.randint(0, len(num_competetors_load) - 1)
@@ -76,79 +75,91 @@ def print_intro():
 
 
 def rr_main(player_name, player_money):
+    console_format.clear()
+
+    return_money = 0
+    rr_continue = True
+
     print_intro()
     if console_format.is_proceed_menu() == False:
-        return 0
-
-    current_competetors = []
-    current_status_of_competetors = []
-    current_num_competetors = 0
-    you_won = False
-
-    current_num_competetors = console_format.input_number(
-        "How many people do you wish to compete with? (5 is reccomended): ", 1, 40)
-    current_max_num_competetors = current_num_competetors
-
-    prize_pool = 0
-
-    for a in range(current_num_competetors):
-        current_competetors.append(rnd.choice(console_format.russian_names))
-        current_status_of_competetors.append(True)
-        prize_pool += rnd.randint(100, 1000)
-    current_competetors.append(player_name)
-    current_status_of_competetors.append(True)
-    print(f"You can win {prize_pool} monis.")
-    print(
-        f"The odds of you surviving could be {round((1 / len(current_competetors) * 100), 2)}%.")
-    console_format.format_reset()
-    input()
+        return return_money
 
     while True:
-        console_format.clear()
-        i = 0
-        magazine = 0
-        magazine = create_magazine(
-            current_competetors, current_status_of_competetors)
-        while i <= current_max_num_competetors:
-            if current_status_of_competetors[i] == True:
-                print_competetor_status(
-                    current_competetors, current_status_of_competetors)
-                print()
-                print(f"{current_competetors[i]}'s turn.", end="")
-                input()
-                console_format.print_loading_bar(10, 0.1)
-                if i == magazine:
-                    current_status_of_competetors[i] = False
-                    current_num_competetors -= 1
+        if rr_continue == False:
+            return return_money
 
-                    console_format.clear()
-                    print_competetor_status(
-                        current_competetors, current_status_of_competetors)
-                    print()
-                    print(Fore.LIGHTRED_EX + f"Bang!!!")
-                    print(Fore.WHITE, end="")
-                    break
-                else:
-                    print(f"The gun didn't fire.")
-                input()
-            console_format.clear()
-            i += 1
-        input()
-
-        if current_num_competetors == 0 and current_status_of_competetors[len(current_competetors) - 1] == True:
-            you_won = True
-            break
-        elif current_status_of_competetors[len(current_competetors) - 1] == False:
+        while True:
+            current_competetors = []
+            current_status_of_competetors = []
+            current_num_competetors = 0
             you_won = False
-            break
-    if you_won == True:
-        win_screen(
-            current_competetors, current_status_of_competetors, prize_pool)
 
-    else:
-        lose_screen(
-            current_competetors, current_status_of_competetors, prize_pool)
-        prize_pool = 0
+            current_num_competetors = console_format.input_number(
+                "How many people do you wish to compete with? (5 is reccomended): ", 1, 40)
+            current_max_num_competetors = current_num_competetors
 
-    input("Press enter to return to the main menu...")
-    return prize_pool
+            prize_pool = 0
+
+            for a in range(current_num_competetors):
+                current_competetors.append(
+                    rnd.choice(console_format.russian_names))
+                current_status_of_competetors.append(True)
+                prize_pool += rnd.randint(100, 1000)
+            current_competetors.append(player_name)
+            current_status_of_competetors.append(True)
+            print(f"You can win {prize_pool} monis.")
+            print(
+                f"The odds of you surviving could be {round((1 / len(current_competetors) * 100), 2)}%.")
+            console_format.format_reset()
+            input()
+
+            while True:
+                console_format.clear()
+                i = 0
+                magazine = 0
+                magazine = create_magazine(
+                    current_competetors, current_status_of_competetors)
+                while i <= current_max_num_competetors:
+                    if current_status_of_competetors[i] == True:
+                        print_competetor_status(
+                            current_competetors, current_status_of_competetors)
+                        print()
+                        print(f"{current_competetors[i]}'s turn.", end="")
+                        input()
+                        console_format.print_loading_bar(10, 0.1)
+                        if i == magazine:
+                            current_status_of_competetors[i] = False
+                            current_num_competetors -= 1
+
+                            console_format.clear()
+                            print_competetor_status(
+                                current_competetors, current_status_of_competetors)
+                            print()
+                            print(Fore.LIGHTRED_EX + f"Bang!!!")
+                            print(Fore.WHITE, end="")
+                            break
+                        else:
+                            print(f"The gun didn't fire.")
+                        input()
+                    console_format.clear()
+                    i += 1
+                input()
+
+                if current_num_competetors == 0 and current_status_of_competetors[len(current_competetors) - 1] == True:
+                    you_won = True
+                    break
+                elif current_status_of_competetors[len(current_competetors) - 1] == False:
+                    you_won = False
+                    break
+            if you_won == True:
+                win_screen(
+                    current_competetors, current_status_of_competetors, prize_pool)
+                return_money += prize_pool
+
+            else:
+                lose_screen(
+                    current_competetors, current_status_of_competetors, prize_pool)
+
+            input()
+            console_format.clear()
+            rr_continue = console_format.want_to_continue("Russian roulette™")
